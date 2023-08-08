43b92bf427f4a8986cb5e1c647ea0587574209ef
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-08-08 13:00:22 +0300

    Updated Klothline smart contract

--- Contract/klothline.sol
+++ Contract/klothline.sol
@@ -2,95 +2,137 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract klothline{
+contract Klothline {
     uint internal productsLength = 0;
     address payable public owner;
-    enum KlothType{none, pants, shirts, dresses, skirts, shorts, shoes, headwear}
+    
+    enum KlothType { None, Pants, Shirts, Dresses, Skirts, Shorts, Shoes, Headwear }
 
-    struct product{
-        KlothType klothtype;
+    struct Product {
+        KlothType klothType;
         string image;
         string name;
         string size; 
         uint price;
-        uint quantity;
         uint stock;
+        uint quantity;
     }
-    mapping (uint => product) internal products;
+    
+    mapping (uint => Product) internal products;
 
+    event ProductAdded(
+        uint indexed productId,
+        KlothType klothType,
+        string image,
+        string name,
+        string size,
+        uint price,
+        uint stock
+    );
+
+    event ProductPurchased(
+        address indexed buyer,
+        uint indexed productId,
+        uint quantity,
+        uint totalPrice
+    );
 
     constructor() payable {
         owner = payable(msg.sender);
     }
 
-
     modifier onlyOwner {
-        require(owner == msg.sender, "only owner can perform this");
+        require(owner == msg.sender, "Only owner can perform this");
         _;
     }
 
-    function addproduct( KlothType _klothtype, string memory _image, string memory _name, string memory _size, uint _price, uint _stock) public onlyOwner {
-        uint _quantity = 0;
-        products[productsLength] = product(
-            _klothtype,
+    function addProduct(
+        KlothType _klothType,
+        string memory _image,
+        string memory _name,
+        string memory _size,
+        uint _price,
+        uint _stock
+    ) public onlyOwner {
+        products[productsLength] = Product(
+            _klothType,
             _image,
             _name,
             _size,
             _price,
-            _quantity,
+            _stock,
+            0
+        );
+        emit ProductAdded(
+            productsLength,
+            _klothType,
+            _image,
+            _name,
+            _size,
+            _price,
             _stock
         );
         productsLength++;
-
     }
 
-
-    function readProduct(uint _index) public view returns(KlothType, string memory, string memory, string memory, uint, uint, uint) {
+    function getProduct(uint _index) public view returns(
+        KlothType,
+        string memory,
+        string memory,
+        string memory,
+        uint,
+        uint,
+        uint
+    ) {
+        require(_index < productsLength, "Invalid product index");
         return (
-            products[_index].klothtype,
+            products[_index].klothType,
             products[_index].image,
             products[_index].name,
             products[_index].size,
             products[_index].price,
-            products[_index].quantity,
-            products[_index].stock
+            products[_index].stock,
+            products[_index].quantity
         );
-
     }
 
-    function getklothtypes() public pure returns (KlothType[] memory) {
+    function getKlothTypes() public pure returns (KlothType[] memory) {
         KlothType[] memory types = new KlothType[](7);
-        types[0] = KlothType.pants;
-        types[1] = KlothType.shirts;
-        types[2] = KlothType.dresses;
-        types[3] = KlothType.skirts;
-        types[4] = KlothType.shorts;
-        types[5] = KlothType.shoes;
-        types[6] = KlothType.headwear;
+        types[0] = KlothType.Pants;
+        types[1] = KlothType.Shirts;
+        types[2] = KlothType.Dresses;
+        types[3] = KlothType.Skirts;
+        types[4] = KlothType.Shorts;
+        types[5] = KlothType.Shoes;
+        types[6] = KlothType.Headwear;
         return types;
     }
 
-    function purchaseproduct(uint _index)public payable{
-
-        require(msg.value >= products[_index].price * products[_index].quantity, "Insufficient funds");
-        require(products[_index].quantity >= products[_index].stock, "We are low on stock");
+    function purchaseProduct(uint _index, uint _quantity) public payable {
+        require(_index < productsLength, "Invalid product index");
+        require(_quantity > 0, "Quantity must be greater than 0");
+        require(_quantity <= products[_index].stock, "Insufficient stock");
         
-        // Perform the purchase
-        if (msg.value > products[_index].price * products[_index].quantity) {
-            uint refundAmount = msg.value - products[_index].price * products[_index].quantity;
+        uint totalPrice = products[_index].price * _quantity;
+        require(msg.value >= totalPrice, "Insufficient funds");
+
+        // Transfer the payment to the shop owner's account
+        owner.transfer(totalPrice);
+
+        // Refund excess payment
+        uint refundAmount = msg.value - totalPrice;
+        if (refundAmount > 0) {
             payable(msg.sender).transfer(refundAmount);
         }
 
-        // Transfer the payment to the shop owner's account
-        uint paymentAmount = products[_index].price * products[_index].quantity;
-        owner.transfer(paymentAmount);
-        products[_index].stock -= products[_index].quantity;
+        // Update product quantity and stock
+        products[_index].quantity += _quantity;
+        products[_index].stock -= _quantity;
+
+        emit ProductPurchased(msg.sender, _index, _quantity, totalPrice);
     }
-
-
 
     function getProductsLength() public view returns (uint) {
-        return (productsLength);
+        return productsLength;
     }
-
 }
